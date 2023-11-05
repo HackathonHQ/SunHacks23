8608dda04a6eeb5f2ca46a161dfaa38cf02efc99
code-wolf-byte <hackerstark37@gmail.com>
2023-11-04 20:14:00 -0700

    added home page

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_page(request):
+    pass
+
+def about_page(request):
+    return render(request, 'pages/about.html')
+
+def tos_page(request):
+    return render(request, 'pages/tos.html')
+
+def privacy_page(request):
+    return render(request, 'pages/privacy.html')
+
